f07f49f94cf6edec11cfc95b3d44ada8d11624bf
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2023-02-06 16:45:27 +0300

    err

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
+import '../Navbar/Navbar.scss'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="wrapper">
+          <div className='left'>
+            <div className="item">
+                <img src="/images/usflg.png" alt="" />
+                <KeyboardArrowDownIcon/>
+            </div>
+            <div className="item">
+                <span>USD</span>
+                <KeyboardArrowDownIcon />
+            </div>
+            <div className="item">
+                <Link to='/products/1'>Women</Link>
+            </div>
+            <div className="item">
+                <Link to='/products/2'>Men</Link>
+            </div>
+            <div className="item">
+                <Link to='/products/3'>Children</Link>
+            </div>
+           </div>
+          <div className='center'>
+            <Link to='/'>SARACOUTURE</Link>
+          </div>
+          <div className='right'>
+             <div className='center'>
+                 <Link to='/'>Homepage</Link>
+             </div>
+             <div className='center'>
+                 <Link to='/'>About</Link>
+             </div>
+             <div className='center'>
+                 <Link to='/'>Contact</Link>
+             </div>
+             <div className='center'>
+                 <Link to='/'>Stores</Link>
+             </div>
+             <div className="icons">
+                <SearchIcon/>
+                <FavoriteBorderIcon/>
+                <PersonIcon/>
+                <div className="cartIcon">
+                    <ShoppingCartIcon/>
+                    <span>0</span>
+                </div>
+             </div>
+          </div>
+        </div>
+    </div>
+  )
+}
